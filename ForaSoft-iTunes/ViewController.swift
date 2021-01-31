ebdefea87962fa8fbd28a0c87dde5fa673652648
AsahiOcean <asahiocean@icloud.com>
2021-02-01 01:32:45 +0500

    2

--- ForaSoft-iTunes/ViewController.swift
+++ ForaSoft-iTunes/ViewController.swift
@@ -1,19 +1,8 @@
-//
-//  ViewController.swift
-//  ForaSoft-iTunes
-//
-//  Created by Sergey Fedotov on 31.01.2021.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
